9959f6136fcc82c2313eaafec516123a0311a17e
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-06 22:03:19 -0300

    Clase Numero 2.
    3 ejercicios de modelo de parcial.
    No estan terminados.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
 
+	var precio;
+	precio=prompt ("100");
+	var	descuento;
+	descuento=prompt ("100*0.10");
+	var IVA;
+	IVA=prompt ("100*1.21");
+	var elPrecioFinal;
+
+	alert()
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	prompt=base("Ingrese base");
+	prompt=altura("Ingrese altura");
+
+	
+
+	alert ("La superficie es "+ superficie" y el perímetro es "+perímetro" ");
+
+
 
 }
