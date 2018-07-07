7844bcb7f8755cf38ac34cf7929b69c0bd20cd45
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-06 22:01:15 -0300

    Clase Numero 2.
    Viernes 6 de Julio.
    4 ejercicios.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,37 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
 
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+
+	resultado=sueldo*1.10;
+	document.getElementById('resultado').value=resultado;
 	
+
+
+	/*
+	
+	50		100%
+	x		10%
+
+	=50*10/100
+
+	Sueldo por 10 / 100
+	
+	Sueldo * 0,10
+
+	Sueldo + Sueldo*0,10
+
+		1		0,10
+
+	=Sueldo * 1,10. Sacamos el 10% de aumento.
+
+	Para un descuento de un 25%
+	=Sueldo * 0,75
+
+
+	*/
 }
