d4cbb2639c55674ed23138b17e008760fc476c81
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-10 22:11:12 -0300

    Clase Nro 3.
    Correccion del ejercicio Nro 7.

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+	var edad;
+	var estCivil;
+	estCivil=document.getElementById('estadoCivil').value;
+	edad=document.getElementById('edad').value;
+	if (edad<18&& estCivil!="Soltero"){
+		alert("Es muy pequeño para NO ser soltero");
+		}
+
+
+}//FIN DE LA FUNCIÓN
