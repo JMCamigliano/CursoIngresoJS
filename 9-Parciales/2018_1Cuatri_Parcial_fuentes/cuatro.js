209b7f2959a200de200dade378df4b6788a57065
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-13 17:18:33 -0300

    Ejercicio Nro 4 - Parcial.
    Realizado completo!

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var resta;
 
+	numeroUno=prompt ("Ingrese el primer número");
+	numeroDos=prompt ("Ingrese el segundo número");
+	
+	if(numeroUno==numeroDos){
+		alert (numeroUno+numeroDos);
+	}
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno>numeroDos){
+		resta=numeroUno-numeroDos
+		alert (resta);
+	}else{
+
+		suma=numeroUno+numeroDos
+		
+		if(suma>10){
+
+			alert ("La suma es "+suma+" y superó el 10");
+		}
+		else{
+
+			alert (suma);}
+		}
+
+	
+		
+		/*if(suma>10){
+
+		alert (La suma es y superó el 10);*/
+		
 }
+	
+
+
+	
+		
+
+
