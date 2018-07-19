6eb7c932b8b8b7dd74fb021f5d5cbcb169aab5aa
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-19 17:52:04 -0300

    Práctica vacaciones.
    Ejercicios del 1 al 3 del PARCIAL.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var elPrecioFinal;
+
+	precio=prompt ("Ingrese el precio");
+	descuento=prompt ("Ingrese el descuento");
+
+	elPrecioFinal = precio - (precio * descuento/ 100);
+
+	document.getElementById('elPrecioFinal').value = elPrecioFinal;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var resultado;
+
+	ancho=prompt("Por favor, ingrese el ancho del rectángulo");
+	largo=prompt("Por favor, ingrese el largo del rectángulo");
+
+	/*ancho=document.getElementById('ancho').value;
+	ancho=parseINT(ancho);
+	largo=document.getElementById('largo').value;
+	largo=parseINT(largo);*/
+
+	resultado=(largo*ancho)*2;
+
+	alert ("El perímetro del rectángulo es " + resultado);
 
 }
