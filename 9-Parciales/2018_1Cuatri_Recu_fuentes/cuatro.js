a43d30651f1def396d4948c21e9ef7b845908408
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-20 18:07:57 -0300

    Práctica - Vacaciones.
    Ejercicio Nro. 4.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,36 @@
 function mostrar()
-{
 
+{
+	var numeroUno;
+	var numeroDos;
+	var division;
+	var suma;
+
+	numeroUno=prompt("Ingrese el primer número");
+	numeroDos=prompt("Ingrese el segundo número");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	suma=numeroUno+numeroDos;
+
+	if (numeroUno==numeroDos) {
+
+		alert(+ numeroUno + "" + numeroDos);
+
+	}
+
+	if (numeroUno>numeroDos) {
+
+		division=numeroUno/numeroDos;
+		alert(division); }
+
+	if (numeroUno<numeroDos){
+		
+		if(suma<50){
+
+			alert ("La suma es "+suma+" y es menor a 50");}
+
+		else{
+
+			alert (suma);}
+	}
 }
