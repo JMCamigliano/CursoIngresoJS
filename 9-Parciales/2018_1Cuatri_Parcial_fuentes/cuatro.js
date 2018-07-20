8da69bddbf1f8e8437efbd0435d25653a278b65c
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-20 18:06:58 -0300

    Práctica - Vacaciones.
    Ejercicio Nro 4.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -7,41 +7,29 @@ function mostrar()
 
 	numeroUno=prompt ("Ingrese el primer número");
 	numeroDos=prompt ("Ingrese el segundo número");
-	
-	if(numeroUno==numeroDos){
-		alert (numeroUno+numeroDos);
-	}
-
 	numeroUno=parseInt(numeroUno);
 	numeroDos=parseInt(numeroDos);
+	suma=numeroUno+numeroDos
+
+	if (numeroUno==numeroDos){
+
+		alert (+ numeroUno + "" + numeroDos);
+	}
 
 	if(numeroUno>numeroDos){
-		resta=numeroUno-numeroDos
-		alert (resta);
-	}else{
+		
+		alert (numeroUno-numeroDos);
+	}
 
-		suma=numeroUno+numeroDos
+	if(numeroUno<numeroDos){
 		
 		if(suma>10){
 
-			alert ("La suma es "+suma+" y superó el 10");
-		}
+			alert ("La suma es "+suma+" y superó el 10");}
+
 		else{
 
 			alert (suma);}
-		}
-
-	
-		
-		/*if(suma>10){
-
-		alert (La suma es y superó el 10);*/
-		
-}
-	
-
-
-	
-		
-
-
+			
+	}	
+}	
