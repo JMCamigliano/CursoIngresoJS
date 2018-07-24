22b0fd14398316d94cf62ded640e281e72bd4d8c
JMCamigliano <40812685+JMCamigliano@users.noreply.github.com>
2018-07-24 17:15:28 -0300

    Práctica - vacaciones.
    SWITCH
    Ejercicios del 1 al 6.

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+ 
+	var mesDelAño;
+	mesDelAño=document.getElementById('mes').value;	
+	switch(mesDelAño){
 
-//alert (mesDelAño);
-	
-	
+		case "Febrero":
+		alert ("Este mes no tiene más de 29 días.");
+		break;
 
+		default:
+		alert ("Este mes tiene 30 o más días");
 
-}//FIN DE LA FUNCIÓN
+	}
+
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,23 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	var mesDelAño;
+	mesDelAño=document.getElementById('mes').value;	
+	switch(mesDelAño){
+
+		case "Febrero":
+		alert ("Este mes tiene 28 días");
+		break;
+
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		alert ("Este mes tiene 30 días");
+		break;
+
+		default:
+		alert ("Este mes tiene 31 días");
+	}
 	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+}
